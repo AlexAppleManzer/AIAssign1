7760d405a244bfbc3a29355cfcc829592485af00
AlexAppleManzer <alexappl3man@gmail.com>
2019-01-16 13:39:26 -0500

    Added Collision Detection

--- main.py
+++ main.py
@@ -32,25 +32,39 @@ class VacuumCleaner:
         root.p = p
         self.vacuum = screen.create_image((x*50+10, y*50+10), image=p, anchor='nw')
 
-    def go_up(self):
-        direction.config(text="UP")
-        screen.move(self.vacuum, 0, -50)
-        self.y -= 1
+    def go_up(self, f):
+        if f.is_square(self.x, self.y - 1):
+            direction.config(text="UP")
+            screen.move(self.vacuum, 0, -50)
+            self.y -= 1
+            return 1
+        else:
+            return 0
 
-    def go_down(self):
-        direction.config(text="DOWN")
-        screen.move(self.vacuum, 0, 50)
-        self.y += 1
+    def go_down(self, f):
+        if f.is_square(self.x, self.y + 1):
+            direction.config(text="DOWN")
+            screen.move(self.vacuum, 0, 50)
+            self.y += 1
+        else:
+            return 0
 
-    def go_left(self):
-        direction.config(text="LEFT")
-        screen.move(self.vacuum, -50, 0)
-        self.x -= 1
+    def go_left(self, f):
+        if f.is_square(self.x - 1, self.y):
+            direction.config(text="LEFT")
+            screen.move(self.vacuum, -50, 0)
+            self.x -= 1
+            return 1
+        else:
+            return 0
 
-    def go_right(self):
-        direction.config(text="RIGHT")
-        screen.move(self.vacuum, 50, 0)
-        self.x += 1
+    def go_right(self, f):
+        if f.is_square(self.x + 1, self.y):
+            direction.config(text="RIGHT")
+            screen.move(self.vacuum, 50, 0)
+            self.x += 1
+        else:
+            return 0
 
     def getpos(self):
         return [self.x, self.y]
@@ -124,20 +138,24 @@ def init_gui():
 
     f = Field(10, 10)
     f.make_square(5, 5)
+    f.make_square(4, 5)
+    f.make_square(5, 4)
+    f.make_square(4, 4)
+
     f.make_dirty(4, 4)
     f.draw()
-    v = VacuumCleaner(0, 0)
+    v = VacuumCleaner(4, 4)
 
-    up = Button(botFrame, text="UP", command=v.go_up)
+    up = Button(botFrame, text="UP", command=lambda: v.go_up(f))
     up.pack(side=LEFT)
 
-    down = Button(botFrame, text="DOWN", command=v.go_down)
+    down = Button(botFrame, text="DOWN", command=lambda: v.go_down(f))
     down.pack(side=LEFT)
 
-    left = Button(botFrame, text="LEFT", command=v.go_left)
+    left = Button(botFrame, text="LEFT", command=lambda: v.go_left(f))
     left.pack(side=LEFT)
 
-    right = Button(botFrame, text="RIGHT", command=v.go_right)
+    right = Button(botFrame, text="RIGHT", command=lambda: v.go_right(f))
     right.pack(side=LEFT)
 
     suck = Button(botFrame, text="SUCK", command=lambda: v.vacuum_suck(f))
